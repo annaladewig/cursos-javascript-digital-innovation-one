5f5afe268455c6a37f18e1f89eda0815c470df51
Anna Ladewig <annamaria.ladewig@gmail.com>
2021-04-07 21:22:35 -0300

    Introdução ao JavaScript

---
+++ js_basico/parte1/js/main.js
@@ -0,0 +1,16 @@
+var nome = "Anna Ladewig";
+var n1 = 25;
+var n2 = 20;
+var frase = "Japão é o melhor time do mundo."
+
+//alert("Bem Vindo(a) " + nome);
+//alert(nome + " tem " + n1 + " anos");
+//alert(n1 + n2);
+
+console.log(nome);
+console.log(nome.toLowerCase());
+console.log(nome.toUpperCase());
+console.log(n1 + n2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+
